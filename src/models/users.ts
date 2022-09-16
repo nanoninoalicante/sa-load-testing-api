5d6b7668cdc535bb006f9e1edbaf921b42a9daa2
Christopher Hill <christopher@nanonino.com>
2022-09-16 17:26:09 +0200

    Added saveUser method

--- src/models/users.ts
+++ src/models/users.ts
@@ -4,7 +4,8 @@ import mongoose, {ObjectId} from 'mongoose';
 import { User } from "../mongoose/connection-v2"
 export const usersSchema = new mongoose.Schema({
     name: String,
-    updatedAt: String
+    updatedAt: String,
+    items: Array
 });
 export const createUser = async (data: any) => {
     const user = new User(data);
@@ -17,6 +18,9 @@ export const updateUser = async (data: any) => {
 
 export const saveUser = async (data: any) => {
     const user = await User.findOne({_id: data._id})
-    user.updatedAt = "test";
-    return await user.save();
+    const user2 = await User.findOne({_id: data._id})
+    user.items = ["test", "new", new Date().valueOf()];
+    await user.save();
+    user2.updatedAt = "new";
+    await user2.save();
 }
