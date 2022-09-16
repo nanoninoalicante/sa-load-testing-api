6cc247d1dd8ee38494eac1e2074aa543ba5fcf7b
Christopher Hill <christopher@nanonino.com>
2022-09-16 17:33:43 +0200

    Skip versioning

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,4 +1,4 @@
-import mongoose, {ObjectId} from 'mongoose';
+import mongoose from 'mongoose';
 // import { mainConnection } from '../mongoose/connection';
 // import { connectNames } from '../mongoose/connection-v2';
 import { User } from "../mongoose/connection-v2"
@@ -6,7 +6,7 @@ export const usersSchema = new mongoose.Schema({
     name: String,
     updatedAt: String,
     items: Array
-});
+}, { skipVersioning: true });
 export const createUser = async (data: any) => {
     const user = new User(data);
     return await user.save();
@@ -17,8 +17,8 @@ export const updateUser = async (data: any) => {
 }
 
 export const saveUser = async (data: any) => {
-    const user = await User.findOne({_id: data._id})
-    const user2 = await User.findOne({_id: data._id})
+    const user = await User.findOne({ _id: data._id })
+    const user2 = await User.findOne({ _id: data._id })
     user.items = ["test", "new", new Date().valueOf()];
     await user.save();
     user2.updatedAt = "new";
